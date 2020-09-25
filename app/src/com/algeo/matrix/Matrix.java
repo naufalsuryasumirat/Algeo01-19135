eb631a5ac37c1210ba39818cbd94847fb4438c7c
Govindabhakta <igovindabhakta@gmail.com>
2020-09-25 11:43:52 +0700

    Added setter and getter, renamed attribute
    Renamed elmt to element
    Added setter and getter methods

--- app/src/com/algeo/matrix/Matrix.java
+++ app/src/com/algeo/matrix/Matrix.java
@@ -3,7 +3,7 @@ package com.algeo.matrix;
 
 public class Matrix {
 
-    double[][] elmt;
+    double[][] element;
 
     int rows;
     int columns;
@@ -13,12 +13,26 @@ public class Matrix {
         System.out.println("Hello world");
     }
 
+    // Constructors
     public void Matrix(int rows, int columns)
     {
         this.rows = rows;
         this.columns = columns;
+        this.element = new double[rows][columns];
     }
 
+    // Getter and Setter methods
+    public double getElmt(int row, int column)
+    {
+        return this.element[row][column];
+    }
+
+    public void setElmt(int row, int column, double val)
+    {
+        this.element[row][column] = val;
+    }
+
+    // Methods
     public void Test()
     {
         System.out.println("Testing TEST function");
