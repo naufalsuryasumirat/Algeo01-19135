2202c4c1152937659c1f8fd2f03e63498b7a01c1
Govindabhakta <igovindabhakta@gmail.com>
2020-09-25 11:33:45 +0700

    Added packages main and matrix
    Main is for GUI and app navigation
    Matrix is for all matrix calculations and operations

---
+++ app/src/com/algeo/main/MainApp.java
@@ -0,0 +1,12 @@
+package com.algeo.main;
+
+import com.algeo.matrix.Matrix;
+
+public class MainApp {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello world");
+        Matrix test = new Matrix();
+        test.Test();
+    }
+}
---
+++ app/src/com/algeo/matrix/Matrix.java
@@ -0,0 +1,26 @@
+package com.algeo.matrix;
+
+
+public class Matrix {
+
+    double[][] elmt;
+
+    int rows;
+    int columns;
+
+    public static void main (String[] args)
+    {
+        System.out.println("Hello world");
+    }
+
+    public void Matrix(int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public void Test()
+    {
+        System.out.println("Testing TEST function");
+    }
+}
