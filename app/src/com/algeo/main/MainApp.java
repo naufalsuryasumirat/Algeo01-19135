0f27d3d317109bd47ecc33792391be033508b6ea
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2020-09-25 12:01:12 +0700

    push kedua (coba-coba)
    push kedua

--- app/src/com/algeo/main/MainApp.java
+++ app/src/com/algeo/main/MainApp.java
@@ -1,10 +1,13 @@
 package com.algeo.main;
 
 import com.algeo.matrix.Matrix;
+import com.algeo.matrix.Determinan;
 
 public class MainApp {
     public static void main (String[] args)
     {
+        Determinan simpleMessageObject = new Determinan();
+        simpleMessageObject.simpleMessage();
         System.out.println("Hello world");
         Matrix test = new Matrix();
         test.Test();
---
+++ app/src/com/algeo/matrix/Determinan.java
@@ -0,0 +1,7 @@
+package com.algeo.matrix;
+
+public class Determinan {
+    public static void simpleMessage(){
+        System.out.println("Method Determinan");
+    }
+}
