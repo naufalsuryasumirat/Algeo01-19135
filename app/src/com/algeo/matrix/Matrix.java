ee2406b378ccf2cbf509e8dae4735e71f178b942
Stefanus Jeremy Aslan <stefanusjeremy@gmail.com>
2020-09-27 20:38:34 +0700

    SPL metode Gauss Jordan beserta metode-metode pendukungnya.
    Metode pendukung ditambahkan di package com.algeo.matrix, sementara SPL
    Gauss Jordan terletak pada class baru bernama 'SPL' di package yang sama.
    
    Metode Gauss Jordan masih bisa menghasilkan hasil yang tidak sesuai
    apabila terdapat kolom yang kosong pada matriks augmented dan masih
    belum ditest secara menyeluruh untuk memastikan kualitas hasil.

--- app/src/com/algeo/matrix/Matrix.java
+++ app/src/com/algeo/matrix/Matrix.java
@@ -1,6 +1,8 @@
 package com.algeo.matrix;
 
 
+import java.util.Scanner;
+
 public class Matrix {
 
     double[][] element;
@@ -59,4 +61,170 @@ public class Matrix {
         return rMatrix;
     }
 
+    public void makeMatrix() {
+        /** ALGORITMA **/
+        Scanner scan = new Scanner(System.in);
+        rows = scan.nextInt();
+        columns = scan.nextInt();
+        element = new double[rows][columns];
+    }
+
+    public void bacaMatrix() {
+        /** KAMUS LOKAL **/
+        int i, j;
+        /** ALGORITMA **/
+        Scanner scan = new Scanner(System.in);
+        for (i = 0; i < this.rows; ++i) {
+            for (j = 0; j < this.columns; ++j) {
+                this.element[i][j] = scan.nextDouble();
+            }
+        }
+    }
+
+    public void bulatElemenKeNol(){
+        // Fungsi yang digunakan untuk membulatkan bilangan positif yang sangat mendekati nol ke nol dalam matriks
+        /** KAMUS LOKAL **/
+        int i, j;
+        /** ALGORITMA **/
+        for (i = 0; i < this.rows; ++i) {
+            for (j = 0; j < this.columns; ++j) {
+                if ((this.element[i][j] < 0.00001 && this.element[i][j] > 0) || (this.element[i][j] > -0.00001 && this.element[i][j] < 0)) {
+                    this.element[i][j] = 0;
+                }
+            }
+        }
+    }
+    public int idxkolomLeadingOne(int row){
+        // I.S. : MATRIKS BERUPA MATRIKS ESELON
+        // FUNGSI MENGEMBALIKAN KOLOM LEADING ONE BERADA DALAM SUATU BARIS.
+        /** KAMUS LOKAL **/
+        int j;
+        /** ALGORITMA **/
+        for (j=0;j<this.columns-1;++j) {
+            if (this.element[row][j] == 1) {
+                return j;
+            }
+        }
+        return -999;
+    }
+    public int idxbarisPersAkhir(){
+        // return baris terakhir yang tidak kosong
+        /** KAMUS LOKAL **/
+        int i,j;
+        /** ALGORITMA **/
+        for (i=this.rows-1;i>=0;--i) {
+            j = 0;
+            while(j < this.columns){
+                if (this.element[i][j]==0){
+                    ++j;
+                }
+                else {
+                    return i;
+                }
+            }
+        }
+        return -999;
+    }
+
+    public void plusAllPL(){
+        //Setiap persamaan linear dijumlahkan dengan semua persamaan linear
+        /** KAMUS LOKAL **/
+        int i,j,k;
+        /** ALGORITMA **/
+        for (i=0; i< this.rows; ++i) {
+            for (j=0; j< this.columns; ++j) {
+                for (k=0; k< this.rows; ++k) {
+                    this.element[i][j] += this.element[k][j];
+                }
+            }
+        }
+    }
+
+    public void TulisMatrix(){
+        /** KAMUS LOKAL **/
+        int i,j;
+        /** ALGORITMA **/
+        for (i=0; i< this.rows; ++i) {
+            System.out.print(this.element[i][0]);
+            for (j=1; j< this.columns;++j) {
+                System.out.print(" "+this.element[i][j]);
+                if ((i != this.rows-1) && (j == this.columns-1)) {
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
+    public void segitigaAtas (boolean[] bariskosong){
+        /** KAMUS LOKAL **/
+        int i,j,k;
+        double ratio;
+        /** ALGORITMA **/
+        for (i=0; i < this.rows; ++i) {
+            if (!bariskosong[i] && this.element[i][i]!= 0) {
+                for (j = 0; j < this.rows; ++j) {
+                    if (j > i) {
+                        ratio = this.element[j][i] / this.element[i][i];
+                        for (k = 0; k < this.columns; ++k) {
+                            this.element[j][k] -= (ratio * this.element[i][k]);
+                        }
+                    }
+                }
+            }
+        }
+        this.bulatElemenKeNol();
+    }
+    public void SAtoEselon(){
+        /** KAMUS LOKAL **/
+        int i,j,k;
+        double divisor;
+        /** ALGORITMA **/
+        for (i=0; i <this.rows; ++i) {
+            divisor = 0;
+            k = 0;
+            while (k<this.columns && divisor ==0) {
+                if(this.element[i][k]!=0) {
+                    divisor = this.element[i][k];
+                }
+                ++k;
+            }
+            if (divisor!=0) {
+                for (j = 0; j < this.columns; ++j) {
+                    this.element[i][j] /= divisor;
+                }
+            }
+        }
+        this.bulatElemenKeNol();
+    }
+
+    public void ReduksiEselon(boolean[] bariskosong, boolean[] kolomkosong){
+        /** KAMUS LOKAL **/
+        int i,j,k;
+        double divisor,ratio;
+        /** ALGORITMA **/
+        for (i = 0; i < this.rows; ++i) {
+            divisor = 0;
+            k = 0;
+            if (!kolomkosong[i] && !bariskosong[i]) {
+                for (j = 0; j < this.rows; ++j) {
+                    if (j > i) {
+                        while (k < this.columns && divisor == 0) {
+                            if (this.element[i][k] != 0) {
+                                divisor = this.element[i][k];
+                            }
+                            ++k;
+                        }
+                        ratio = this.element[i][j] / divisor;
+                        for (k = 0; k < this.columns; ++k) {
+                            this.element[i][k] -= (ratio * this.element[j][k]);
+                        }
+                    }
+                }
+            }
+        }
+        this.bulatElemenKeNol();
+    }
+
+    public void sortBarisBasedOnColumnVal (int column){
+    }
 }
---
+++ app/src/com/algeo/matrix/SPL.java
@@ -0,0 +1,170 @@
+package com.algeo.matrix;
+
+public class SPL {
+    Matrix matrixAugmented = new Matrix();
+    public void splGaussJordan() {
+        matrixAugmented.makeMatrix();
+        matrixAugmented.bacaMatrix();
+
+        boolean isTidakAdaJawaban = false;
+        boolean[] bariskosong = new boolean[matrixAugmented.rows];
+        boolean[] kolomkosong = new boolean[matrixAugmented.columns-1];
+        double x[] = new double[matrixAugmented.columns]; // nilai X apabila solusi unik
+        int i,j,k;
+        ;
+        // Apabila matrixAugmented.element augmented memiliki baris X dan kolom X-1
+
+        // Menjumlahkan setiap baris sebagai upaya untuk menghindari elemen 0
+        matrixAugmented.plusAllPL();
+
+        //Matriks Segitiga Atas
+        // SPL pasti lebih dari 1 persamaan
+        matrixAugmented.segitigaAtas(bariskosong);
+
+        //Matriks Segitiga Atas -> Eselon
+        matrixAugmented.SAtoEselon();
+
+        //Matriks Eselon -> Matriks Eselon Tereduksi
+        matrixAugmented.ReduksiEselon(bariskosong,kolomkosong);
+
+
+        // Pengecekkan terdapat jawaban atau tidak
+        for (i=0; i<matrixAugmented.rows; ++i) {
+            if (matrixAugmented.idxkolomLeadingOne(i) == -999 && matrixAugmented.element[i][matrixAugmented.columns - 1] != 0) {
+                isTidakAdaJawaban = true;
+            }
+
+        }
+        // Pengisian array boolean bariskosong dan kolomkosong
+        for (i=0;i< matrixAugmented.rows;++i){
+            bariskosong[i] = true;
+        }
+        for (j=0;j< matrixAugmented.columns-1;++j){
+            kolomkosong[j] = true;
+        }
+        // Mengecek apakah terdapat baris atau kolom yang kosong
+        for (i=0; i< matrixAugmented.rows; ++i) {
+            for (j=0; j< matrixAugmented.columns; ++j) {
+                if (matrixAugmented.element[i][j] != 0) {
+                    bariskosong[i] = false;
+                }
+            }
+        }
+        for (j=0; j<matrixAugmented.columns-1; ++j) {
+            for (i=0; i< matrixAugmented.rows; ++i) {
+                if (matrixAugmented.element[i][j] != 0) {
+                    kolomkosong[j] = false;
+                }
+            }
+        }
+
+        // Tulis matriks augmented dalam bentuk matriks eselon tereduksi
+        matrixAugmented.TulisMatrix();
+
+        // Pemrosesan nilai
+        System.out.print("\n");
+        if (!isTidakAdaJawaban) {
+            System.out.print("Solusi SPL:\n");
+            for (i = 0; i <=matrixAugmented.idxbarisPersAkhir(); ++i) {
+                if (!kolomkosong[i]) {
+                    x[i] = matrixAugmented.element[i][matrixAugmented.columns - 1];
+                    System.out.print("x" + (i + 1) + " =");
+                    if (x[i]!=0) {
+                        System.out.print(" " + x[i]);
+                    }
+                    if (matrixAugmented.idxkolomLeadingOne(matrixAugmented.idxbarisPersAkhir()) != matrixAugmented.columns-2) {
+                        for (k = matrixAugmented.idxkolomLeadingOne(matrixAugmented.idxbarisPersAkhir())+1; k < matrixAugmented.columns - 1; ++k) {
+                            if (matrixAugmented.element[i][k]!=0 && k != i-1){
+                                System.out.print(" -" + "(" + matrixAugmented.element[i][k] + ")" + "x" + (k + 1));
+                            }
+                        }
+                    }
+                    else if (x[i]==0){
+                        System.out.print(" " + "x" + (j + 1));
+                        System.out.print("\n");
+                    }
+                    System.out.print("\n");
+                }
+
+                else{
+                    System.out.print("x" + (i + 1) + " = " + null + "\n");
+                }
+            }
+            for (j = matrixAugmented.idxbarisPersAkhir()+1; j < matrixAugmented.columns-1; ++j) {
+                if (!kolomkosong[j]) {
+                    System.out.print("x" + (j + 1) + " = " + "x" + (j + 1));
+                    System.out.print("\n");
+                }
+                else {
+                    System.out.print("x" + (i + 1) + " = " + null + "\n");
+                }
+            }
+        }
+        else {
+            System.out.println("Tidak ada solusi");
+        }
+    }
+
+    public void splGauss(){
+        matrixAugmented.makeMatrix();
+        matrixAugmented.bacaMatrix();
+
+        boolean isTidakAdaJawaban = false;
+        boolean[] bariskosong = new boolean[matrixAugmented.rows];
+        boolean[] kolomkosong = new boolean[matrixAugmented.columns-1];
+        double x[] = new double[matrixAugmented.columns]; // nilai X apabila terdapat 1 solusi
+        int i,j,k;
+        ;
+        // Apabila matrixAugmented.element augmented memiliki baris X dan kolom X-1
+
+        // Menjumlahkan setiap baris sebagai upaya untuk menghindari elemen 0
+        matrixAugmented.plusAllPL();
+
+        //Matriks Segitiga Atas
+        // SPL pasti lebih dari 1 persamaan
+        matrixAugmented.segitigaAtas(bariskosong);
+
+        //Matriks Segitiga Atas -> Eselon
+        matrixAugmented.SAtoEselon();
+
+        // Pengecekkan terdapat jawaban atau tidak
+        for (i=0; i<matrixAugmented.rows; ++i) {
+            if (matrixAugmented.idxkolomLeadingOne(i) == -999 && matrixAugmented.element[i][matrixAugmented.columns - 1] != 0) {
+                isTidakAdaJawaban = true;
+            }
+
+        }
+        // Pengisian array boolean bariskosong dan kolomkosong
+        for (i=0;i< matrixAugmented.rows;++i){
+            bariskosong[i] = true;
+        }
+        for (j=0;j< matrixAugmented.columns-1;++j){
+            kolomkosong[j] = true;
+        }
+        // Mengecek apakah terdapat baris atau kolom yang kosong
+        for (i=0; i< matrixAugmented.rows; ++i) {
+            for (j=0; j< matrixAugmented.columns; ++j) {
+                if (matrixAugmented.element[i][j] != 0) {
+                    bariskosong[i] = false;
+                }
+            }
+        }
+        for (j=0; j<matrixAugmented.columns-1; ++j) {
+            for (i=0; i< matrixAugmented.rows; ++i) {
+                if (matrixAugmented.element[i][j] != 0) {
+                    kolomkosong[j] = false;
+                }
+            }
+        }
+
+        // Tulis matriks augmented dalam bentuk matriks eselon tereduksi
+        matrixAugmented.TulisMatrix();
+    }
+    public void Kramer(){
+
+    }
+    public void Balikan(){
+
+    }
+
+}
